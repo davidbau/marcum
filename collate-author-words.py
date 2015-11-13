aaeeb512a25826b55db3ff7c213698a499352f59
David Bau <david.bau@gmail.com>
2015-11-13 13:39:09 -0500

    Collate directories also.

--- collate-author-words.py
+++ collate-author-words.py
@@ -2,9 +2,11 @@
 # Example input:
 # git log -p -U0 --date=raw | python ../collate-author-words.py > ../author_rank
 
-import sys, string, re, math, heapq, sgt, operator
+import sys, string, re, math, heapq, sgt, operator, subprocess
 from datetime import datetime
 from pprint import pprint
+
+cmd = "git --git-dir linux-history/.git log -p -U0 --date=raw".split(' ')
 
 # reads the output of `git log -p -U0 --date=raw`
 
@@ -13,8 +15,18 @@ class CommitRecord:
     self.commit_id = None
     self.author = None
     self.date = None
+    self.changes = []
+
+class ChangeRecord:
+  def __init__(self, new_filename, old_filename):
+    self.new_filename = new_filename
+    self.old_filename = old_filename
     self.add_terms = set()
     self.del_terms = set()
+
+
+def dir_from_filename(f):
+  return f.split('/', 1)[0]
 
 replace_punctuation = string.maketrans(
     string.punctuation, ' ' * len(string.punctuation))
@@ -37,7 +49,7 @@ re_c_string = re.compile(r'''(?x)   # verbose mode
 def accumulate_terms(text, tset,
       skipcomment=True, genericstring=True, genericdigit=True):
   if skipcomment:
-    if re.match(r'^\s*\* ', text):
+    if re.match(r'^\s*\*\s', text):
       return
     text = text.split('/*')[0]
     text = text.split('//')[0]
@@ -50,10 +62,11 @@ def accumulate_terms(text, tset,
   tset.update(text.split())
 
 def interesting_file(filename):
-  return filename.endswith('.c') or filename.endswith('.h')
+  return (filename.endswith('.c') or filename.endswith('.h')) and (not '/staging/' in filename)
 
 def readrecord(inp):
   current_record = None
+  current_change = None
   old_filename = None
   current_filename = None
   while True:
@@ -73,12 +86,21 @@ def readrecord(inp):
       current_record.month = (dt.year - 1998) * 12 + (dt.month - 1)
     elif line.startswith('--- '):
       old_filename = line.split(' ', 1)[1].strip()
+      if old_filename.startswith('a/'):
+        old_filename = old_filename[2:]
     elif line.startswith('+++ '):
       current_filename = line.split(' ', 1)[1].strip()
-    elif line.startswith('+') and interesting_file(current_filename):
-      accumulate_terms(line[1:], current_record.add_terms)
-    elif line.startswith('-') and interesting_file(old_filename):
-      accumulate_terms(line[1:], current_record.del_terms)
+      if current_filename.startswith('b/'):
+        current_filename = current_filename[2:]
+      if interesting_file(current_filename):
+        current_change = ChangeRecord(current_filename, old_filename)
+        current_record.changes.append(current_change)
+      else:
+        current_change = None
+    elif line.startswith('+') and current_change:
+      accumulate_terms(line[1:], current_change.add_terms)
+    elif line.startswith('-') and current_change:
+      accumulate_terms(line[1:], current_change.del_terms)
 
 class UnigramModel:
   def __init__(self, d=None):
@@ -122,34 +144,63 @@ class AuthorData:
     self.name = name
     self.language_model = UnigramModel()
     self.document_count = 0
+    self.commits = []
   def add_document(self, words):
     self.language_model.update(words)
     self.document_count += 1
-    
+  def add_commit(self, commit):
+    self.commits.append(commit)
 
 language_model = UnigramModel()
 author_data = {}
-limit = 200000
-for rec in readrecord(sys.stdin):
+dir_data = {}
+limit = 2000
+count = 0
+
+gp = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+for rec in readrecord(gp.stdout):
+  count += 1
+  print >> sys.stderr, 'record', count, rec.commit_id
   # pprint(vars(rec))
-  if len(rec.add_terms):
+  if len(rec.changes):
     author = rec.author
     if author not in author_data:
       author_data[author] = AuthorData(author)
     ad = author_data[author]
-    ad.add_document(rec.add_terms)
-    language_model.update(rec.add_terms)
-    limit -= 1
-    if limit <= 0:
-      break
+    ad.add_commit(rec)
+    for change in rec.changes:
+      language_model.update(change.add_terms)
+      ad.add_document(change.add_terms)
+
+      topdir = dir_from_filename(change.new_filename)
+      if topdir not in dir_data:
+        dir_data[topdir] = AuthorData(topdir)
+      dd = dir_data[topdir]
+      dd.add_document(change.add_terms)
+  if count >= limit:
+    break
 
 for word in language_model.top_words(30):
   print word, language_model.p(word), "(%f bits)" % language_model.bits(word)
 print 'Total entropy', language_model.entropy()
+
 author_rank = sorted([
   (ad, language_model.cross_entropy(ad.language_model))
   for ad in author_data.values()], key=operator.itemgetter(1))
+
 for (ad, xe) in author_rank:
   if ad.document_count >= 10:
     print '%s has cross-entropy %f after %d docs' % (
           ad.name, xe, ad.document_count)
+
+dir_rank = sorted([
+  (dd, language_model.cross_entropy(dd.language_model))
+  for dd in dir_data.values()], key=operator.itemgetter(1))
+
+for (dd, xe) in dir_rank:
+  if dd.document_count >= 10:
+    print 'dir %s has cross-entropy %f after %d docs' % (
+          dd.name, xe, dd.document_count)
+
+
